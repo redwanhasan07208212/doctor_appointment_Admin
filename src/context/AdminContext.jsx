f93d79f6b597857b7f01169350effe3752af844d
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-02-22 17:56:21 +0600

    payment system done

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -11,6 +11,7 @@ const AdminContextProvider = (props) => {
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
   );
   const [doctors, setDoctors] = useState([]);
+  const [appointments, setAppointments] = useState([]);
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const getAllDoctors = async () => {
@@ -50,6 +51,22 @@ const AdminContextProvider = (props) => {
       console.log(err);
     }
   };
+
+  const getAllAppointments = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/admin/appointments", {
+        headers: { Authorization: `Bearer ${aToken}` },
+      });
+      if (data.success) {
+        setAppointments(data.appointments);
+        console.log(data.appointments);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
   const value = {
     aToken,
     setAToken,
@@ -57,6 +74,9 @@ const AdminContextProvider = (props) => {
     doctors,
     getAllDoctors,
     changeAvailibility,
+    appointments,
+    setAppointments,
+    getAllAppointments,
   };
 
   return (
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -5,7 +5,60 @@ import { createContext } from "react";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-  const value = {};
+  const calculateAge = (dob) => {
+    if (!dob) {
+      return "N/A"; // Return a default value if dob is missing
+    }
+
+    const today = new Date();
+    const birthDate = new Date(dob);
+
+    // Check if the date is valid
+    if (isNaN(birthDate.getTime())) {
+      return "Invalid Date"; // Return a message for invalid dates
+    }
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    // Adjust age if the birthday hasn't occurred yet this year
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+    if (
+      monthDifference < 0 ||
+      (monthDifference === 0 && today.getDate() < birthDate.getDate())
+    ) {
+      age--;
+    }
+
+    return age;
+  };
+
+  const months = [
+    " ",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split("_");
+    return (
+      dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+    );
+  };
+
+  const value = {
+    calculateAge,
+    slotDateFormat,
+  };
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
